be78754f2317c2c4b313b3f89aea95010bf1c723
Eduardo Amroginski Santos <eduardo-amroginski@ibm.com>
2021-10-26 16:13:49 -0300

    Adicionando arquivo de teste de Rotas usando a extensão REST CLIENT

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -1,9 +1,13 @@
 import express from "express";
 const router = express.Router();
 
-router.get("/", (request, response) => {});
+router.get("/", (request, response) => {
+  response.send("REST API ok");
+});
 
-router.get("/:id", (request, response) => {});
+router.get("/:id", (request, response) => {
+  response.send("REST API ID ok");
+});
 
 router.post("/", (request, response) => {});
 
