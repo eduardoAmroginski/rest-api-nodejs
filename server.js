cdc38c3e54f5c330b81f68ea5f0d7b209d7ba04b
Eduardo Amroginski Santos <eduardo-amroginski@ibm.com>
2021-10-26 15:50:02 -0300

    Criando conexão com o Banco de dados usando o Mongoose

--- server.js
+++ server.js
@@ -5,6 +5,21 @@ const app = express();
 const port = 3000;
 const host = "localhost";
 
+mongoose.connect("mongodb://localhost/subscribers", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on("error", (err) => {
+  console.log("Database Error: " + err);
+});
+
+db.once("open", () => {
+  console.log("Database Connected");
+});
+
 app.listen(3000, () => {
   console.log(`Server Running at http://${host}:${port}`);
 });
